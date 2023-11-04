6359079ed3e79c47d7da6096f6c3ee101799f81f
Arjun <arjundasback515@gmail.com>
2023-11-04 17:12:28 +0600

    build: create not found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundpage = () => {
+    return (
+        <div>
+            <h1 className='text-xl font-bold text-red-700 text-center'>404 page not found</h1>
+        </div>
+    );
+};
+
+export default NotFoundpage;
